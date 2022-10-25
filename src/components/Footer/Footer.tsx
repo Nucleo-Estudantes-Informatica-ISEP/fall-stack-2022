002fe9d66bc9f4b6d74be3cdcab716ed3ff8c633
Eduardoooxd <ecouto93@gmail.com>
2022-10-25 01:14:50 +0100

    feat: adding initial stylings to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,22 @@
 import { FunctionComponent } from 'react';
+import './Footer.scss';
 interface FooterProps {}
 
 const Footer: FunctionComponent<FooterProps> = () => {
-    return <>Footer</>;
+    return (
+        <footer className="footer__container">
+            <div className="footer__last__edition__container" id="last-edition-wrapper">
+                <div className="footer__last__edition__animation">
+                    <a
+                        className="footer__last__edition__link"
+                        href="https://fallstack2020.nei-isep.org"
+                        target="_blank"
+                        rel="noreferrer">
+                        <div className="footer__last__edition__link__text">Edição Anterior</div>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 };
 export default Footer;
