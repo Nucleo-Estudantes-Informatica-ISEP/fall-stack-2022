83aa4c2116f5c8b4ae57d1c81cb7ca012d6d86a8
Tomás Lopes <tomas050302@gmail.com>
2022-11-08 21:40:39 +0000

    feat(modal): added modal animation on hide & show

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { X } from 'react-bootstrap-icons';
 import ModalProps from '../../types/ModalProps';
 
 const Modal: React.FC<ModalProps> = ({ hidden, setHidden, title, bodyText }) => {
+    useEffect(() => {
+        const onKeyPress = (e: KeyboardEvent) => {
+            if (!hidden && e.key === 'Escape') setHidden(true);
+        };
+
+        window.addEventListener('keydown', onKeyPress);
+        return () => window.removeEventListener('keydown', onKeyPress);
+    }, [hidden, setHidden]);
+
     return (
-        <>
-            {!hidden && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-                    <div className="relative my-6 mx-auto w-auto max-w-3xl">
-                        <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
-                            <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-                                <h3 className="w-full text-center text-3xl font-semibold capitalize">
-                                    {title}
-                                </h3>
-                                <button
-                                    className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none focus:outline-none"
-                                    onClick={() => setHidden(true)}>
-                                    {<X className="text-red-600" />}
-                                </button>
-                            </div>
+        <div
+            className={`
+            ${hidden ? 'hidden' : 'fixed'} 
+            inset-0 animate-fade-imm bg-gray-700/60
+            transition-opacity`}>
+            <div className="z-1 fixed inset-0 flex items-center justify-center overflow-y-auto overflow-x-hidden shadow-lg outline-none focus:outline-none">
+                <div className="relative my-6 mx-auto w-auto max-w-3xl">
+                    <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+                        <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+                            <h3 className="w-full text-center text-3xl font-semibold capitalize">
+                                {title}
+                            </h3>
+                            <button
+                                className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none focus:outline-none"
+                                onClick={() => setHidden(true)}>
+                                {<X className="text-red-600" />}
+                            </button>
+                        </div>
 
-                            <div className="mx-auto flex w-3/4 flex-row items-center justify-around pt-4">
-                                <li className="b-y-black b-solid cursor-pointer list-none text-lg">
-                                    Tab
-                                </li>
-                                <li className="b-y-black b-solid cursor-pointer list-none text-lg">
-                                    Tab
-                                </li>
-                                <li className="b-y-black b-solid cursor-pointer list-none text-lg">
-                                    Tab
-                                </li>
-                            </div>
+                        <div className="mx-auto flex w-3/4 flex-row items-center justify-around pt-4">
+                            <li className="b-y-black b-solid cursor-pointer list-none text-lg">
+                                Tab
+                            </li>
+                            <li className="b-y-black b-solid cursor-pointer list-none text-lg">
+                                Tab
+                            </li>
+                            <li className="b-y-black b-solid cursor-pointer list-none text-lg">
+                                Tab
+                            </li>
+                        </div>
 
-                            <div className="relative flex-auto py-6 px-12">
-                                <p className="mb-4 text-lg leading-relaxed text-slate-500">
-                                    {bodyText}
-                                </p>
-                            </div>
+                        <div className="relative flex-auto py-6 px-12">
+                            <p className="mb-4 text-lg leading-relaxed text-slate-500">
+                                {bodyText}
+                            </p>
                         </div>
                     </div>
                 </div>
-            )}
-        </>
+            </div>
+        </div>
     );
 };
 
--- src/types/ModalProps.ts
+++ src/types/ModalProps.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default interface ModalProps {
     hidden: boolean;
     setHidden: React.Dispatch<React.SetStateAction<boolean>>;
