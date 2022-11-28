25312c98eb07573800500a8cfc4e0a1defdbf1c8
Tomás Lopes <tomas050302@gmail.com>
2022-11-28 01:16:59 +0000

    enhancement(event-infos): improved responsiveness

--- src/components/Company/index.tsx
+++ src/components/Company/index.tsx
@@ -16,7 +16,7 @@ const Company: React.FC<CompanyProps> = ({ logoHref, name, modalBodyText }) => {
                 className="flex min-h-[8rem] cursor-pointer items-center justify-center transition duration-300 ease-in-out hover:scale-105 lg:min-h-[11rem]"
                 onClick={() => setIsHidden(false)}>
                 <img
-                    className="h-full max-h-44 w-full max-w-[15rem] object-cover lg:max-h-28  lg:max-w-[18rem] xl:max-h-32 xl:max-w-[16rem]"
+                    className="h-full max-h-36 w-full max-w-[12rem] object-cover lg:max-h-28  lg:max-w-[18rem] xl:max-h-32 xl:max-w-[16rem]"
                     src={logoHref}
                     alt={name}
                 />
