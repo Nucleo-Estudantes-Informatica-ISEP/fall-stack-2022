9ecac3b6a80cce922b2302dcdf3d7ef9ce109bac
Tomás Lopes <tomas050302@gmail.com>
2022-11-28 00:51:27 +0000

    enhancement(sponsors): added hover scale animation

--- src/components/Sponsor/index.tsx
+++ src/components/Sponsor/index.tsx
@@ -11,7 +11,7 @@ const Sponsor: React.FC<SponsorProps> = ({ logoHref, name, website }) => {
         <a
             target="_blank"
             href={website}
-            className="m-auto my-2 flex h-52 w-52 items-center justify-center"
+            className="m-auto my-2 flex h-52 w-52 items-center justify-center transition duration-300 ease-in-out hover:scale-105"
             rel="noreferrer">
             <img
                 className="my-4 mx-auto block h-auto max-h-[90%] max-w-[90%] rounded-[5px]"
