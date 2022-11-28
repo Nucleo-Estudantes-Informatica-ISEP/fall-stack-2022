eda47d3941018d644f99da5a8acc763c6ae3f7d8
Tomás Lopes <tomas050302@gmail.com>
2022-11-28 00:49:27 +0000

    enhancement(info-text): justified text

--- src/components/InfoText/index.tsx
+++ src/components/InfoText/index.tsx
@@ -17,26 +17,20 @@ const InfoText: React.FC<InfoTextProps> = ({ days, month, beginningTime, endTime
             <EventInfos days={days} month={month} beginningTime={beginningTime} endTime={endTime} />
 
             <EventDescription>
-                <p>
+                <p className="text-justify">
                     Mais um ano e mais uma edição do evento FallStack no Instituto Superior de
-                    Engenharia do Porto!
-                </p>
-                <p>
-                    Este evento que já vai na sua 4ª edição, será realizado nos dias{' '}
-                    {days.join(' e ')} de {month} e está de volta ao formato presencial, com entrada
-                    gratuita, tendo por objetivo fomentar o contacto direto entre empresas e alunos
-                    finalistas do curso de Engenharia Informática.
-                </p>
-                <p>
+                    Engenharia do Porto! Este evento que já vai na sua 4ª edição, será realizado nos
+                    dias {days.join(' e ')} de {month} e está de volta ao formato presencial, com
+                    entrada gratuita, tendo por objetivo fomentar o contacto direto entre empresas e
+                    alunos finalistas do curso de Engenharia Informática.
+                    <br />
                     No primeiro dia poderás contar com as entrevistas às empresas participantes,
                     onde ficarás a saber mais sobre as mesmas, desde as áreas em que trabalham até
                     propostas de estágio.
-                </p>
-                <p>
+                    <br />
                     No segundo dia irão ocorrer as Round Tables, onde poderás interagir diretamente
                     com as empresas e conhecê-las com mais detalhe.
-                </p>
-                <p>
+                    <br />
                     Contamos com a tua presença para este evento que te vai ajudar e esclarecer
                     dúvidas que tenhas sobre empresas da nossa área de estudos.
                 </p>
